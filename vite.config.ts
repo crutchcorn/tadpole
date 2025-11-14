b7a22944ad7cfbb5fd5d82a616c8539a65f73d62
Corbin Crutchley <git@crutchcorn.dev>
2025-11-14 11:20:55 -0800

    chore: add Router

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
-export default defineConfig({ plugins: [cloudflare(), react()] });
+export default defineConfig({
+  plugins: [
+    // cloudflare(),
+    tanstackRouter({
+      generatedRouteTree: "client-src/routeTree.gen.ts",
+      routesDirectory: "client-src/routes",
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
+});
